4be7940db585498261ff0714c03d1f420fc5ef9e
Taksh Joshi <takshjoshi@Takshs-MacBook-Pro.local>
2024-12-10 11:06:02 +0530

    Auth

---
+++ prototypeV1/prototypeV1Tests/prototypeV1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  prototypeV1Tests.swift
+//  prototypeV1Tests
+//
+//  Created by Taksh Joshi on 29/11/24.
+//
+
+import Testing
+@testable import prototypeV1
+
+struct prototypeV1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
